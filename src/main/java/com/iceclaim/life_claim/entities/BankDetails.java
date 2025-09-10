f3f12df293465ee9edc30d7ab8cd631c1ecf8d2f
Pg-0102 <guptapayal88312@gmail.com>
2025-09-10 12:01:33 +0530

    Used embedded.

--- src/main/java/com/iceclaim/life_claim/entities/BankDetails.java
+++ src/main/java/com/iceclaim/life_claim/entities/BankDetails.java
@@ -1,4 +1,18 @@
 package com.iceclaim.life_claim.entities;
 
+import jakarta.persistence.Embeddable;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Embeddable
 public class BankDetails {
+    private String bankName;
+    private String accountNumber;
+    private String accountName;
 }
--- src/main/java/com/iceclaim/life_claim/entities/Claims.java
+++ src/main/java/com/iceclaim/life_claim/entities/Claims.java
@@ -1,4 +1,22 @@
 package com.iceclaim.life_claim.entities;
 
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Claims {
+    @Id
+    private UUID id;
+
+    @Embedded
+    private BankDetails bankDetails;
 }
