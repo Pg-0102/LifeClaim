5e9eb45cfc148c3b22a19fe0f8ff4f4476e1a1ff
DB-100 <dhawalbhedasgaonkar222@gmail.com>
2025-09-10 11:25:20 +0530

    Database connection set

--- src/main/java/com/iceclaim/life_claim/entities/Policy.java
+++ src/main/java/com/iceclaim/life_claim/entities/Policy.java
@@ -1,4 +1,17 @@
 package com.iceclaim.life_claim.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Policy {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+    private String policyNumber;
+
 }
